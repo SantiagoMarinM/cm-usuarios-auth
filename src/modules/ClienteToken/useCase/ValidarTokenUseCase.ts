802249b53c7d57c37833aa61346598ddb724e77f
SantiagoMarinM <smarin@coordinadora.com>
2025-03-26 17:22:44 -0500

    feat: se realiza prueba 1 para desplegar

--- src/modules/ClienteToken/useCase/ValidarTokenUseCase.ts
+++ src/modules/ClienteToken/useCase/ValidarTokenUseCase.ts
@@ -14,8 +14,6 @@ export default class ValidarTokenUseCase {
     );
 
     async execute(id_cliente: string, token: string): Promise<void> {
-        console.log('id_cliente>>', id_cliente);
-        console.log('token>>', token);
         const cliente = await this.validarIdClienteUseCase.execute(id_cliente);
         if (!cliente) throw new UNAUTHORIZED('Acceso no autorizado', '401', CLIENTE_INVALIDO);
         if (!cliente.activo) throw new UNAUTHORIZED('Acceso no autorizado', '401', CLIENTE_INACTIVO);
