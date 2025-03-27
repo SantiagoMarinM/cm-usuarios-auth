375300ee57f1d1f354e97f99328f544e24d4f35a
SantiagoMarinM <smarin@coordinadora.com>
2025-03-27 10:08:07 -0500

    fix: se agregan logs de seguimiento

--- src/infrastructure/bd/postgresql/dao/querys/ClienteTokenQuery.ts
+++ src/infrastructure/bd/postgresql/dao/querys/ClienteTokenQuery.ts
@@ -2,4 +2,4 @@ export const insertCliente = `INSERT INTO clientes (alias, nombre, disparador_pu
                  VALUES ('REP', $1, true, true, now(), $2, $3, $4)
                  RETURNING id`;
 
-export const selectValidarCliente = `SELECT activo, tiempo_expiracion FROM santiago.clientes_autorizados WHERE codigo_empleado = $1`;
+export const selectValidarCliente = `SELECT activo FROM santiago.clientes_autorizados WHERE codigo_empleado = $1`;
